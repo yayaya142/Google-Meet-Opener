6e8bd505c3903ed063b7e6338e82b6ad30e8e326
shai <yayaya142@gmail.com>
2024-03-07 22:11:38 +0200

    add main and open_web_api

---
+++ main.py
@@ -0,0 +1,14 @@
+import web_api
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print("Hello World")
+    web_api.open_web_api()
+    
---
+++ web_api.py
@@ -0,0 +1,14 @@
+
+import webbrowser
+
+def open_web_api():
+    # URL = 'https://www.meet.new/'
+    URL = 'https://www.example.com/'
+    
+    webbrowser.open(URL)
+
+
+
+
+def get_web_url():
+    return 'https://www.example.com/'
