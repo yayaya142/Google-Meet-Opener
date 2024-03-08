cff90ec1ae34791269ed9198438aa9f7ba88c30d
shai <yayaya142@gmail.com>
2024-03-08 17:43:14 +0200

    change to classes add PREFIX

--- web_api.py
+++ web_api.py
@@ -1,23 +1,57 @@
 import webbrowser
 import subprocess
 
-# this api is used to open the web browser and create a new meeting
-def open_web_api():
-    URL = 'https://www.meet.new/'  
-    webbrowser.open(URL)
+
+# this class control the web api 
+class WebApi:
+    # URL = 'meet.new'
+    # PREFIX = "meet"
+    # base_url = "https://meet.google.com/"
+    URL = 'https://www.example.com/'
+    PREFIX = "Example"
+    base_url = "https://testing.example.com/"
+    
+
+    # this api is used to open the web browser and create a new meeting
+    def open_web_api():
+        webbrowser.open(WebApi.URL)
+       
+    # this api is used to get the url of the open window
+    def get_open_web_url_api():
+        URL = get_open_url_with_power_shell()
+        if (URL is None):
+            return None
+        URL = WebApi.extract_meeting_id(URL)
+        if (URL is None):
+            return None
+        URL = WebApi.create_meeting_invite(URL)
+        if (URL is None):
+            return None
+        
+        return URL
+
+    # Define the extract_meeting_id function
+    def extract_meeting_id(title):
+        if title.startswith(WebApi.PREFIX) == False:
+            return None
+        
+        parts = title.split(' - ')  
+        if len(parts) > 1:
+            return parts[1] 
+        else:
+            return None  
     
     
+    # this function is used to create a meeting invite
+    def create_meeting_invite(key):
+        if key is None:
+            return None
+        return WebApi.base_url + key
     
+
+
     
-# this api is used to get the url of the open window
-def get_open_web_url_api():
-    URL = get_open_url_with_power_shell()
-    return URL
-
-
-
-
-# this function use the powershell to get the url of the open window
+    # this function use the powershell to get the url of the open window
 def get_open_url_with_power_shell():
         # set the powershell command
     powershell_command = r"powershell.exe -Command Get-Process | Where-Object {$_.MainWindowTitle -like '*Google Chrome*'} | Select-Object MainWindowTitle"
@@ -30,26 +64,8 @@ def get_open_url_with_power_shell():
         output_lines = result.stdout.splitlines()
     else:
         return None
-    
-    URL = extract_meeting_id(output_lines[3])
-    URL = create_meeting_invite(URL)    
-    
-    return URL
+        
+   
+    return output_lines[3]
 
 
-# this function is used to extract the meeting id from the title of the window
-def extract_meeting_id(title):
-    parts = title.split(' - ')  # split the string by ' - '
-    if len(parts) > 1:
-        return parts[1]  # return the second part
-    else:
-        return None  # return None if there's no ' - ' in the strings
-    
-    
-    
-# this function is used to create a meeting invite
-def create_meeting_invite(key):
-    base_url = "https://meet.google.com/"
-    if key is None:
-        return None
-    return base_url + key
